a88b06d71fc2f4b063d3a9f935e2f945bc3e7684
Rauha201 <rauhauddin1@gmail.com>
2026-06-18 16:42:26 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,16 @@
+class ContestStats{  
+    int[] scores ={85,90,95};
 
+
+    void PrintFirstScore(){
+        System.out.println("First score: "+ scores[0]);
+    } 
+}
+
+public class Main1{
+    public static void main(String[] args){
+        ContestStats contest = new ContestStats();
+        contest.PrintFirstScore();
+
+    }
+}
