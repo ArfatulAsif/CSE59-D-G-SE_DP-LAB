6f89fbe02eeeb5b202a8d49c91ed0e5f6a157816
Jaman_Tahsid <jamanmahid49@gmail.com>
2026-06-18 16:02:08 +0600

    Add ContestStats class to print first score

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,13 @@
+class ContestStats{
+    int[] arr={85,90,95};
+    void printFirstScore(){
+        System.out.println("Value at first index : "+arr[0]);
+    }
+} 
 
+public class Main1 {
+      public static void main(String[] args) {
+         ContestStats obj=new  ContestStats();
+        obj.printFirstScore();
+    }  
+}
