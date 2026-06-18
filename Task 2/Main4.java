26c37ad7d2df700ed0e9ba46ac21aaeeab17c281
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-18 17:01:12 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,20 @@
+classs Sensor{
+        String Name;
+        int batteryLevel;
 
+        void change() {
+            batteryLevel = 100;
+            System.out.println(x: "Sensor fully charged");
+        }
+    }
+public class Main4 {
+    public statics void main(string[] args){
+        Sensor sensor = new Sensor();
+        sensor.name = "Temperature Sensor";
+        sensor.batteryLevel = 20 ;
+
+        sensor.charge();
+        System.out.println("Battery Level: " +sensor.batteryLevel);
+    }
+
+    }
