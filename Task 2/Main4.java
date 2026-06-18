06e3f189fae732d8620f7fafb8e1660d41fb3f5a
Dwarm-A <abdurrahimarmaan058@gmail.com>
2026-06-18 16:27:53 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,15 @@
-
+class sensor{
+    String name;
+    int batterLevel;
+    void charge(int set){
+        this.batterLevel=set;
+        System.out.println("Sensor fully charged");
+    }
+}
+public class Main4 {
+    public static void main(String[] args) {
+        sensor fein = new sensor();
+        fein.charge(100);
+    }
+    
+}
