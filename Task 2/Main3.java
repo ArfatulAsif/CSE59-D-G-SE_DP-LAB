208b18d5ecc6fcdd674f2512e8710092eb5f3bbe
Dwarm-A <abdurrahimarmaan058@gmail.com>
2026-06-18 16:22:34 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,14 @@
-
+class Countdown{
+    void start(){
+        for(int i= 5; i>0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        Countdown fade= new Countdown();
+        fade.start();
+    }
+}
