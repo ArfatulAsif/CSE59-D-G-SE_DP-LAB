d3d6342911490a99d2803ba55b68a0e38f8fa2c8
Navanahany222-115-244 <navanaalamhany03@gmail.com>
2026-06-18 16:54:06 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,17 @@
 
+class Printer{
+    void printdata(String text)
+    {
+        System.out.println(text);
+    }
+    void printdata(int number){
+        System.out.println(number);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.printdata("hello world");
+        printer.printdata(123);
+    }
+}
