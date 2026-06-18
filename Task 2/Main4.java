71fbdd62a241b331a9f7903b522d8200c274546c
aloycs277 <aloycs277@gmail.com>
2026-06-18 16:14:56 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,19 @@
+class Sensor{
+    String name;
+    int batteryLevel;
+    void  charge (){
+        batteryLevel =100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+public class main4 {
+    public static void main(String[] args) {
+        Sensor s= new Sensor();
+        s.name = "Temperature Sensor";
+        s.batteryLevel = 20;
+        s.charge();
+        System.out.println("Battery Level: " + s.batteryLevel);
+    }
+}
 
