16505534da9012de5f1d71088c75f759e44a12dc
Jaman_Tahsid <jamanmahid49@gmail.com>
2026-06-18 16:49:14 +0600

    Add Countdown class with start method

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,15 @@
-
+class Countdown{
+    void start(){
+        for(int i =5 ; i>0;i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+     Countdown messi =new Countdown();
+     messi.start();
+    }
+ 
+}
