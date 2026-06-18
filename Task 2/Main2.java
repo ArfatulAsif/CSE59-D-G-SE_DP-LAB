2c4bccd402249d41ed2d4b672f3af2e81b031084
rehnuma232-115-266 <rehnumahosainsara@gmail.com>
2026-06-18 16:11:10 +0600

    Add Numberchecker class to check even and >10

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,18 @@
+class Numberchecker{
+	void check(int num)
+	{
+		boolean result = (num%2==0)&&(num>10);
+		System.out.println(result);
+	}
+}
+public class main2 {
+
+	public static void main(String[] args) {
+		Numberchecker checker = new Numberchecker();
+		 checker.check(14);
+		
+
+	}
+
+}
 
