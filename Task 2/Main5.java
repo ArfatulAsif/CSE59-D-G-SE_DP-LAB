3e5a4823be936c1d123fd4a725a6c6f91e5669ac
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-18 17:01:49 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,17 @@
 
+class Printer {
+     void printData(String text){
+        System.out.println(text);   
+      }
+      void printData(int number){
+        System.out.println(number);
+      }
+}
+public class Main5 {
+    public statics void main(String[] ags){
+        Printer printer = new Printer();
+         
+        printer.printerData(text: "Hello,world!");
+        printer.printerData(number: 123);
+    }
+}
