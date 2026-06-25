be90a1c6edcaf4eaf5372a2b2b28f2191c6e4178
Jaman_Tahsid <jamanmahid49@gmail.com>
2026-06-25 17:00:16 +0600

    Implement Shape interface with Rectangle and Circle classes

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,44 @@
+interface Shape{
+    double calculateArea();
+}
+class Rectangle implements Shape{
+    public double length;
+    public double width;
+    public Rectangle(double length,double width){
+        this.length = length;
+        this.width = width;
+    }
+    public double calculateArea(){
+        return length*width;
+    }
+}
+class Circle implements Shape{
+    public double radius;
+    public Circle(double radius){
+        this.radius= radius;
+    }
+    public double calculateArea(){
+        return Math.PI *radius*radius;
+    }
+}
+class Areacalculator {
+    public double calculateTotalArea(Shape[] shapes){
+        double area = 0;
+        for (Shape shape : shapes){
+            area+=shape.calculateArea();
+        }
+        return area;
+    }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        Shape[] shapes = {
+            new Rectangle(5,4),
+            new Circle(3)
+        };
+        Areacalculator calculator = new Areacalculator();
+        System.out.println("Total Area = " +
+        calculator.calculateTotalArea(shapes));
+    }
+}
 
