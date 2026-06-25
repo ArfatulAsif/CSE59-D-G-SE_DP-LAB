8a0ed303d4d394ae0554112ea3e56bd5cac5ba09
Jarin Tahsin Trina <jarintahsintrina@gmail.com>
2026-06-25 17:36:52 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,59 @@
+package com.mycompany.lab3;
+interface Shape {
+    double calculateArea();
+}
+
+class Rectangle implements Shape {
+    double length;
+    double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
+class Circle implements Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class AreaCalculator {
+    double calculateTotalArea(Shape[] shapes) {
+        double area = 0;
+          for (Shape shape : shapes) {
+            area += shape.calculateArea();
+        }
+
+        return area;
+    }
+}
+        
+
+
+public class task2 {
+    public static void main(String[] args) {
+        Shape[] shapes = {
+            new Rectangle(length 5, width 4),
+            new Circle(3)
+        };
+
+        AreaCalculator calculator = new AreaCalculator();
+
+        System.out.println("Total Area = " +
+                calculator.calculateTotalArea(shapes));
+    }
+}
+    
+
 
