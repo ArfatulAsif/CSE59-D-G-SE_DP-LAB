1cdb7b04f9873984dafe9cc9c57904beb8ed119b
rehnuma232-115-266 <rehnumahosainsara@gmail.com>
2026-07-02 17:28:10 +0600

    Implement message sending via Email and SMS

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,49 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+class EmailSender implements MessageSender {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+class SMSSender implements MessageSender {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender messageSender;
+
+    
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+
+
+public class main3 {
+    public static void main(String[] args) {
+
+       
+        MessageSender email = new EmailSender();
+        NotificationService notification1 = new NotificationService(email);
+        notification1.alertUser("Welcome to our system!");
+
+        
+        MessageSender sms = new SMSSender();
+        NotificationService notification2 = new NotificationService(sms);
+        notification2.alertUser("Your OTP is 123456");
+    }
+}
 
