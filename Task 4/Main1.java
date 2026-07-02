dfef6e5e1b7d6bbdd062d0ea730d081d17cc30c7
sharifkhan-dev <sk657966@gmail.com>
2026-07-02 16:35:15 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,37 @@
+class TeamMember {
+    protected String name;
+    
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
+
+class Employee extends TeamMember {
+    public Employee(String name) {
+        super(name);
+    }
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+class Contractor extends TeamMember {
+    public Contractor(String name) {
+        super(name);
+    }
+    
+    public void work() {
+        System.out.println("Contractor is working on the project.");
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Rahim");
+        emp.calculateBonus();
+        
+        Contractor con = new Contractor("Karim");
+        con.work();
+    }
+}
 
