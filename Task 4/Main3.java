f174a65070c38c6fe4fb6f6fd5a8482487d077bc
Yeakub Ali Shakib <yashakib.dev@gmail.com>
2026-07-02 17:14:32 +0600

    Implement message sending via email and SMS

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,49 @@
+public class Main {
+ 
+    
+    interface MessageSender {
+        void sendMessage(String message);
+    }
+ 
+    static class EmailSender implements MessageSender {
 
+    
+        public void sendMessage(String message) {
+            System.out.println("Sending email: " + message);
+        }
+    }
+ 
+
+    static class SMSSender implements MessageSender {
+ 
+    
+        public void sendMessage(String message) {
+            System.out.println("Sending SMS: " + message);
+        }
+    }
+ 
+    
+    static class NotificationService {
+ 
+        private MessageSender messageSender;
+ 
+        public NotificationService(MessageSender messageSender) {
+            this.messageSender = messageSender;
+        }
+ 
+        public void alertUser(String msg) {
+            messageSender.sendMessage(msg);
+        }
+    }
+ 
+    public static void main(String[] args) {
+ 
+        MessageSender email = new EmailSender();
+        NotificationService service1 = new NotificationService(email);
+        service1.alertUser("Welcome!");
+ 
+        MessageSender sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alertUser("OTP: 123456");
+    }
+}
