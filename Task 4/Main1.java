f0d57b65b1533c3923b68f6133dd513792382319
ahmadhussantamim <aht2560@gmail.com>
2026-07-02 16:24:57 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,36 @@
+class TeamMember {
+    protected String name;
 
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
+
+// Employee gets bonus
+class Employee extends TeamMember {
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+// Contractor does not have bonus method
+class Contractor extends TeamMember {
+
+    public Contractor(String name) {
+        super(name);
+    }
+}
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice");
+        Contractor con = new Contractor("Bob");
+        emp.calculateBonus();
+    }
+}
