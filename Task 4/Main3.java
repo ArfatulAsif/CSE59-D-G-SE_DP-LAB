a8d1737bfc5229c06ee5ee9676ae9cadec0cc623
aloycs277 <aloycs277@gmail.com>
2026-07-02 16:29:56 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,35 @@
+interface MessageSender{
+    void sendMessage(String message);
+
+}
+class EmailSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+class NotificationService{
+    private MessageSender sender;
+    NotificationService(MessageSender sender ){
+        this.sender=sender;
+    }
+
+    public void alertUser ( String message){
+        sender.sendMessage(message);
+            
+
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+
+        NotificationService notification = new NotificationService(email);
+
+        notification.alertUser("Hello User!");
+    }
+}
 
