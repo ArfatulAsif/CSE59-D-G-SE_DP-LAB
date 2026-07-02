e9df3f683b462c60eec993c2c89f5ddde9d98c2f
ahmadhussantamim <aht2560@gmail.com>
2026-07-02 16:26:46 +0600

    Implement MessageSender interface with Email and SMS

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,39 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+// Email implementation
+class EmailSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+// SMS implementation (can be added later)
+class SMSSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+// High-level class depends on abstraction
+class NotificationService {
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService service = new NotificationService(sender);
+        service.alertUser("hello User!");
+
+    }
+}
