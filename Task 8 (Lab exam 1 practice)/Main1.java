ee47d0e47363472f35eb4be90908d5f6031a7f67
AbdulJulilShahria <156626375+AbdulJulilShahria@users.noreply.github.com>
2026-07-23 16:02:55 +0600

    Implement student grading and scoring system
    Added functionality to track highest scorer and count top grades.

--- Task 8 (Lab exam 1 practice)/Main1.java
+++ Task 8 (Lab exam 1 practice)/Main1.java
@@ -1,1 +1,35 @@
 
+import java.util.Scanner;
+
+class Course{
+    String highestName="";
+    double highestMarks=0;
+    int topCount=0;
+
+    void addStudent(String name, double marks){
+        if(marks>=80){
+            topCount++;
+        }
+    }
+   String getHighestScorer(){
+    return highestName+ "("+(int)highestMarks+")";
+
+    }
+    int countTopGrades(double threshhold){
+        return topCount++;
+    }
+}
+public class Main1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.next();
+        Course course= new Course();
+        course.addStudent(input.next(),input.nextDouble() );
+        course.addStudent(input.next(),input.nextDouble() );
+        course.addStudent(input.next(),input.nextDouble() );
+
+        System.out.println("highest Scoorer: "+ course.getHighestScorer());
+        System.out.println("number of A+(80+): "+course.countTopGrades(80));
+
+    }
+}
